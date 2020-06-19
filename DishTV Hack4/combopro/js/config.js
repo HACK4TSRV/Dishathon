e4a40ac5a606bf09dfc8ccefaf79777cfe2a8b30
HACK4TSRV <67150200+HACK4TSRV@users.noreply.github.com>
2020-06-19 17:45:02 +0530

    first

---
+++ DishTV Hack4/combopro/js/config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+
+// Make a copy of this file and save it as config.js (in the js directory).
+
+// Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+// Do not include the trailing slash. See the README for more information:
+
+var SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
+
+// OR, if you have not set up a web server that runs the learning-opentok-php code,
+// set these values to OpenTok API key, a valid session ID, and a token for the session.
+// For test purposes, you can obtain these from https://tokbox.com/account.
+
+var API_KEY = '46794344';
+var SESSION_ID = '1_MX40Njc5NDM0NH5-MTU5MjU1MzM1ODU1OX5FT1g4azZwR0l6bHlRVGIrZjk5N2xRNUV-fg';
+var TOKEN = 'T1==cGFydG5lcl9pZD00Njc5NDM0NCZzaWc9YzZmNmRkYzViM2M3MWJmNjk0NmZkYzFiOTlkMGU3ZDk4OWM5MGY5YzpzZXNzaW9uX2lkPTFfTVg0ME5qYzVORE0wTkg1LU1UVTVNalUxTXpNMU9EVTFPWDVGVDFnNGF6WndSMGw2YkhsUlZHSXJaams1TjJ4Uk5VVi1mZyZjcmVhdGVfdGltZT0xNTkyNTUzMzg0Jm5vbmNlPTAuMzQxNjQzNzM3NzgyMTA3MSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTkzMTU4MTgyJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9';
